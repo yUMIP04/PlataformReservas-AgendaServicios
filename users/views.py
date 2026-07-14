79fe35392fda4f62f882c119659d3b69a1b76665
yUMIP04 <yumip5719@gmail.com>
2026-07-14 15:11:31 -0600

    -Creando vista de users.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+
+    if request.method == 'GET':
+        return render(request, 'users/index.html')
+
+    elif request.method == 'POST':
+
+        nombre = request.POST['']
+        apellido = request.POST['']
+        correo = request.POST[''] 
+        clave = request.POST['']
+        clave_confirm = request.POST['']
+        rol = request.POST['']
+
+    return render(request, 'users/index.html')
