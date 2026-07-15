27e4412209bdb148b65d29cf44f194495d2435af
yUMIP04 <yumip5719@gmail.com>
2026-07-15 13:24:05 -0600

    -Crando el formulario de registro.

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Usuario
 # Create your views here.
 
 
@@ -10,11 +10,17 @@ def index(request):
 
     elif request.method == 'POST':
 
-        nombre = request.POST['']
-        apellido = request.POST['']
-        correo = request.POST[''] 
-        clave = request.POST['']
-        clave_confirm = request.POST['']
-        rol = request.POST['']
+        nombre = request.POST['nombre_usuario']
+        apellido = request.POST['apellido_usuario']
+        correo = request.POST['correo_usuario'] 
+        clave = request.POST['password']
+        clave_confirm = request.POST['password_confirm']
+        rol = request.POST['rol']
 
+        if clave_confirm == clave:
+
+            Usuario.objects.create(nombre=nombre, apellido=apellido, correo=correo, clave=clave, rol=rol)
+            print("🥳Se inserto correctamente el usuario")
+        
+        
     return render(request, 'users/index.html')
