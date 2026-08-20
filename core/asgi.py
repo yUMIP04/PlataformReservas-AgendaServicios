0e4125e43d7a8841093bf99bcb238254a28cbed0
yUMIP04 <yumip5719@gmail.com>
2026-08-20 14:59:43 -0600

    -Agregando e investigando sobre la funcion de websocket para meter notificaciones.

--- core/asgi.py
+++ core/asgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_asgi_application()
+django_asgi_app=get_asgi_application()
+
+application = get_asgi_application({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(URLRouter())
+})
